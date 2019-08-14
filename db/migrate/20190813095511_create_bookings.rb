187758ede8e40183bfec7c47924a214cbb83f7aa
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-14 19:21:59 +0900

    booking seeds updated

--- db/migrate/20190813095511_create_bookings.rb
+++ db/migrate/20190813095511_create_bookings.rb
@@ -5,7 +5,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
       t.text :description
       t.datetime :event_start
       t.datetime :event_end
-      t.string :status, default: 'Pending'
+      t.string :status
       t.references :user, foreign_key: true
       t.references :band, foreign_key: true
 
