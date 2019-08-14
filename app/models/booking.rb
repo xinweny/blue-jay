45d5cb34c7c9a216d0b5eb9372bfb87b901ab28f
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-14 23:08:52 +0900

    added total price functionality

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :band
 
-  enum status: [:pending, :accepted, :rejected]
+  enum status: %i[pending accepted rejected]
+
+  def total_price
+    format('%.2f', (event_end - event_start) * band.price / 3600)
+  end
 end
