67704967949922678990674650fc6ca7f880e373
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-14 14:38:50 +0900

    index debugged

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,5 +1,6 @@
 class BandsController < ApplicationController
   before_action :set_band, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @bands = policy_scope(Band)
