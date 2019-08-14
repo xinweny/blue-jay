ca748e9fa61e3a2def908e3c0b5c14811497faba
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-14 17:10:57 +0900

    pundit setup done and new booking form made

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,7 +1,29 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
+
+  def index?
+    user_is_owner?
+  end
+
+  def show?
+    user_is_owner?
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    record.user == user
   end
 end
