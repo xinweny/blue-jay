4b1e6a6d7588ea3206a7b231dc94f4274984cc6b
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-14 16:42:42 +0900

    pundit setup

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+  private
+
+  def booking_params
+    params.require(:booking).permit(:address, :description, :event_start, :event_end)
+  end
 end
