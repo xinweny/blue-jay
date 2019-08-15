17e9812e5a8d0621306e720d4207a4d0e7f521c1
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-15 13:35:15 +0900

    form hints added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_024647) do
+ActiveRecord::Schema.define(version: 2019_08_15_042704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2019_08_15_024647) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "image"
+    t.text "tags"
+    t.text "samples"
     t.index ["user_id"], name: "index_bands_on_user_id"
   end
 
