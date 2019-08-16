2a5149a5060ce58abafdef1c7319372b8cbe7872
Xin Wen Yap <yapxinwen1608@gmail.com>
2019-08-16 17:01:51 +0900

    booking dynamics fixed

--- app/javascript/packs/calculate_price.js
+++ app/javascript/packs/calculate_price.js
@@ -1,19 +1,24 @@
-
-
 const eventStart = document.querySelector('#booking_event_start');
 const eventEnd = document.querySelector('#booking_event_end');
+console.log(eventStart);
+console.log(eventEnd);
 
-let start = "";
-eventStart.addEventListener('blur', (event) => {
+let start = undefined;
+let end = undefined;
+
+eventStart.addEventListener('change', (event) => {
   start = event.target.value;
   let dateStart = new Date(start);
 
   calculatePrice(dateStart, end);
+  flatpickr(".datepicker_end", { allowInput: true, enableTime: true, minDate: dateStart })
 });
 
-let end = "";
-eventEnd.addEventListener('blur', (event) => {
+eventEnd.addEventListener('change', (event) => {
+  console.log(event);
+  console.log(event.target);
   end = event.target.value;
+  console.log(end);
   let dateEnd = new Date(end);
 
   calculatePrice(dateEnd, start);
@@ -26,7 +31,8 @@ function calculatePrice(initDate, newTime) {
   if (newTime !== undefined) {
     let newDate = new Date(newTime);
     let totalTime = Math.abs((initDate - newDate) / 3600000);
-    let totalPrice = totalTime * priceValue;
+    let totalPrice = Math.round(totalTime * priceValue * 100) / 100;
+    console.log(totalPrice);
     priceDiv.innerHTML = `<p><b>Total price:</b> $${totalPrice}</p>`;
   }
 }
