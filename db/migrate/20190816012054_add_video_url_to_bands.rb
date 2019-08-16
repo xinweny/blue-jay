5848c8cc12082ad98bd8aa29d1203eb0ff0dddf4
Carmen Chung <jlchung416@gmail.com>
2019-08-16 12:32:20 +0900

    cards aligned

---
+++ db/migrate/20190816012054_add_video_url_to_bands.rb
@@ -0,0 +1,5 @@
+class AddVideoUrl < ActiveRecord::Migration[5.2]
+  def change
+    add_column :bands, :video_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_152958) do
+ActiveRecord::Schema.define(version: 2019_08_16_012054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 2019_08_15_152958) do
     t.bigint "user_id"
     t.string "image"
     t.text "tags"
-    t.string "video_url"
+    t.text "samples"
     t.string "audio"
+    t.string "video_url"
     t.index ["user_id"], name: "index_bands_on_user_id"
   end
 
